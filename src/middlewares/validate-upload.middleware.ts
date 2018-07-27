09a7d95bd35e1f4fbd18dcb0cb57f5bdf5ab9be9
Mark Wecke <wecke@neoskop.de>
2018-07-27 09:13:15 +0200

    fix: fix wildcards in supported media types

--- src/middlewares/validate-upload.middleware.ts
+++ src/middlewares/validate-upload.middleware.ts
@@ -2,21 +2,18 @@ import { Middleware, UnsupportedMediaTypeError } from '@neoskop/nem';
 import { NextFunction, Request, Response } from 'express';
 import { UPLOAD_ACCPTED_TYPES } from '../tokens';
 import { Inject } from '@angular/core';
+import typeis from 'type-is';
 const debug = require('debug')('hostit:middlewares:validate-upload');
 
 @Middleware()
 export class ValidateUpload {
-    protected readonly acceptedTypes : Set<string>;
-    
-    constructor(@Inject(UPLOAD_ACCPTED_TYPES) acceptedTypes : string[]) {
-        this.acceptedTypes = new Set(acceptedTypes);
+    constructor(@Inject(UPLOAD_ACCPTED_TYPES) protected readonly acceptedTypes : string[]) {
     }
     
     async use(request: Request, _response: Response, next: NextFunction) {
         const type = request.header('content-type')!;
         
-        
-        if(!this.acceptedTypes.has(type)) {
+        if(!typeis(request, this.acceptedTypes)) {
             debug(request.method, request.path, 'INVALID', type);
             return next(new UnsupportedMediaTypeError());
         }
